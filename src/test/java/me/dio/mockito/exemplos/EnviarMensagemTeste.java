941e9343ce09b1fccca83d11e1ab712d20631d5b
SuayMack <pry_chu@hotmail.com>
2023-01-12 01:21:37 -0300

    update Spy EnviarMensagem

--- src/test/java/me/dio/mockito/exemplos/EnviarMensagemTeste.java
+++ src/test/java/me/dio/mockito/exemplos/EnviarMensagemTeste.java
@@ -3,10 +3,11 @@ package me.dio.mockito.exemplos;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verify;
 
 /**
  * Teste da classe {@link EnviarMensagem} exemplificando Spy
@@ -14,6 +15,21 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 public class EnviarMensagemTeste {
 
+    @Spy
+    private EnviarMensagem enviarMensagem;
+
+    @Test
+    void verificarComportamentoDaClasse() {
+        Mockito.verifyNoInteractions(enviarMensagem);
+
+        Mensagem mensagem = new Mensagem("Hello World");
+        enviarMensagem.adicionarMensagem(mensagem);
+
+        Mockito.verify(enviarMensagem).adicionarMensagem(mensagem);
+        Assertions.assertFalse(enviarMensagem.getMensagens().isEmpty());
+    }
+
+    /*
     @Spy
     EnviarMensagem enviarMensagem = new EnviarMensagem();
 
@@ -26,6 +42,6 @@ public class EnviarMensagemTeste {
         verify(enviarMensagem).adicionarMensagem(mensagem);
 
         Assertions.assertEquals(1, enviarMensagem.getMensagens().size());
-    }
+    } */
 
 }
